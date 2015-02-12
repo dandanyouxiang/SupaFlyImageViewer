1a11952896165a9aab032908b0a2e46cc76a76db
refactorsaurusrex <nick@refactorsaurusrex.com>
2015-02-11 23:23:29 -0600

    Basic UI implementation

---
+++ SupaFlyImageViewer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SupaFlyImageViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var args = Environment.GetCommandLineArgs();
+            var uri = new Uri(@"D:\Dropbox\Pictures\Eye-Fi\1-26-2015\DSC00779.JPG");
+            MainImage.Source = new BitmapImage(uri);
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                Close();
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+    }
+}

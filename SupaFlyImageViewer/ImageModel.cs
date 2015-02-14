7b7f32f87fe5580ad49cd94f9b64cba4af90b11a
refactorsaurusrex <nick@refactorsaurusrex.com>
2015-02-14 14:07:01 -0600

    Image zoom animation polish

--- SupaFlyImageViewer/ImageModel.cs
+++ SupaFlyImageViewer/ImageModel.cs
@@ -8,5 +8,15 @@ namespace SupaFlyImageViewer
 {
     public class ImageModel
     {
+
+        public string MyPath
+        {
+            get { return @"D:\Dropbox\Pictures\Eye-Fi\1-26-2015\DSC00779.JPG"; }
+        }
+
+        public int Width
+        {
+            get { return 1000; }
+        }
     }
 }
